abdd0a05969d9aba9df197d698ff6445e1719abd
Marion Andermatt <marion.andermatt@gmx.ch>
2023-03-15 21:59:27 +0100

    intial push second try

--- src/main/java/ch/uzh/ifi/hase/soprafs23/Application.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/Application.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class Application {
 
 
+
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
