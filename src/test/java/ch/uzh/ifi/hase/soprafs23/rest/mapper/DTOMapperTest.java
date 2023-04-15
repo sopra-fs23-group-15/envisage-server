990cfda7c97dbbd8ecd282973efcb201c263016c
Marion Andermatt <marion.andermatt@gmx.ch>
2023-04-15 14:45:50 +0200

    more DTOMapper Tests

--- src/test/java/ch/uzh/ifi/hase/soprafs23/rest/mapper/DTOMapperTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/rest/mapper/DTOMapperTest.java
@@ -1,10 +1,8 @@
 package ch.uzh.ifi.hase.soprafs23.rest.mapper;
 
-import ch.uzh.ifi.hase.soprafs23.entity.Lobby;
-import ch.uzh.ifi.hase.soprafs23.entity.Player;
-import ch.uzh.ifi.hase.soprafs23.rest.dto.LobbyGetDTO;
-import ch.uzh.ifi.hase.soprafs23.rest.dto.PlayerGetDTO;
-import ch.uzh.ifi.hase.soprafs23.rest.dto.PlayerPostDTO;
+import ch.uzh.ifi.hase.soprafs23.constant.GameStatus;
+import ch.uzh.ifi.hase.soprafs23.entity.*;
+import ch.uzh.ifi.hase.soprafs23.rest.dto.*;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -22,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class DTOMapperTest {
 
     @Test
-    public void testCreateLobby_fromEntity_toLobbyGetDTO_success() {
+    public void test_fromEntity_toLobbyGetDTO_success() {
         Lobby lobby = new Lobby();
         List<Player> playerList = new ArrayList<>();
 
@@ -37,7 +35,7 @@ class DTOMapperTest {
     }
 
     @Test
-    public void testPlayerInput_fromPlayerPostDTO_toEntity_success() {
+    public void test_fromPlayerPostDTO_toEntity_success() {
         PlayerPostDTO playerPostDTO = new PlayerPostDTO();
         playerPostDTO.setUserName("Gertrude");
 
@@ -47,7 +45,7 @@ class DTOMapperTest {
     }
 
     @Test
-    public void testPlayer_fromEntity_toPlayerGetDTO_success() {
+    public void test_fromEntity_toPlayerGetDTO_success() {
         Player player = new Player();
         Lobby lobby = new Lobby();
         lobby.setPin(12345678L);
@@ -63,6 +61,84 @@ class DTOMapperTest {
         assertEquals(player.getId(), playerGetDTO.getId());
         assertEquals(player.getLobby().getPin(), playerGetDTO.getLobbyId());
     }
+
+    @Test
+    public void test_fromEntity_toPlayerImageDTO(){
+        PlayerImage playerImage = new PlayerImage();
+        Player player = new Player();
+        player.setUserName("Gertrude");
+        playerImage.setId(3L);
+        playerImage.setPlayer(player);
+        playerImage.setVotes(3);
+
+        PlayerImageDTO playerImageDTO = DTOMapper.INSTANCE.convertEntityToPlayerImageDTO(playerImage);
+
+        assertEquals(playerImageDTO.getId(), playerImage.getId());
+        assertEquals(playerImageDTO.getPlayer(), playerImage.getPlayer().getUserName());
+        assertEquals(playerImageDTO.getVotes(), playerImage.getVotes());
+    }
+
+    @Test
+    public void test_fromEntity_toRoundDTO(){
+        Round round = new Round();
+        List<PlayerImage> playerImages = new ArrayList<>();
+        round.setRoundNumber(5);
+        round.setPlayerImages(playerImages);
+
+        RoundDTO roundDTO = DTOMapper.INSTANCE.convertEntityToRoundDTO(round);
+
+        assertEquals(roundDTO.getRoundNumber(), round.getRoundNumber());
+        assertEquals(roundDTO.getPlayerImages(), round.getPlayerImages());
+
+    }
+
+    @Test
+    public void test_fromEntity_toPlayerScoreDTO(){
+        PlayerScore playerScore = new PlayerScore();
+        Player player = new Player();
+        player.setUserName("Albert");
+        playerScore.setPlayer(player);
+        playerScore.setScore(4);
+
+        PlayerScoreDTO playerScoreDTO = DTOMapper.INSTANCE.convertEntityToPlayerScoreDTO(playerScore);
+
+        assertEquals(playerScoreDTO.getPlayer(), playerScore.getPlayer().getUserName());
+        assertEquals(playerScoreDTO.getScore(), playerScore.getScore());
+    }
+
+    @Test
+    public void test_fromEntity_GameDTO(){
+        Game game =  new Game();
+        List<Round> roundList = new ArrayList<>();
+        List<PlayerScore> playerScoreList = new ArrayList<>();
+        Lobby lobby = new Lobby();
+        lobby.setPin(12345678L);
+        game.setRounds(roundList);
+        game.setPlayerScores(playerScoreList);
+        game.setLobby(lobby);
+        game.setStatus(GameStatus.READY);
+
+        GameDTO gameDTO = DTOMapper.INSTANCE.convertEntityToGameDTO(game);
+
+        assertEquals(gameDTO.getRounds(), game.getRounds());
+        assertEquals(gameDTO.getPlayerScores(), game.getPlayerScores());
+        assertEquals(gameDTO.getLobbyPin(), game.getLobby().getPin());
+        assertEquals(gameDTO.getStatus(), game.getStatus());
+
+    }
+
+    @Test
+    public void test_fromPlayerScoreDTO_toEntity(){
+        PlayerScoreDTO playerScoreDTO = new PlayerScoreDTO();
+        playerScoreDTO.setPlayer("Gertrude");
+        playerScoreDTO.setScore(23);
+
+        PlayerScore playerScore = DTOMapper.INSTANCE.convertPlayerScoreDTOtoEntity(playerScoreDTO);
+
+        assertEquals(playerScore.getPlayer().getUserName(), playerScoreDTO.getPlayer());
+        assertEquals(playerScore.getScore(), playerScoreDTO.getScore());
+    }
+
 
 
 }
