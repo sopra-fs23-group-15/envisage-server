63065a9b2b4374bf07529c5da64088d706e4758f
Marion Andermatt <marion.andermatt@gmx.ch>
2023-04-24 22:06:55 +0200

    LobbyRepositoryTest

---
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/repository/LobbyRepositoryTest.java
@@ -0,0 +1,71 @@
+package ch.uzh.ifi.hase.soprafs23.repository;
+
+import ch.uzh.ifi.hase.soprafs23.constant.EnvisageConstants;
+import ch.uzh.ifi.hase.soprafs23.entity.Lobby;
+import ch.uzh.ifi.hase.soprafs23.entity.Player;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@DataJpaTest
+class LobbyRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private LobbyRepository lobbyRepository;
+
+    @Test
+    public void findByPin_success() {
+
+       Lobby lobby = new Lobby();
+       List<Player> playerList = new ArrayList<>();
+       lobby.setPin(12345678L);
+       lobby.setRoundDuration(EnvisageConstants.DEFAULT_ROUND_DURATION_IN_SECONDS);
+       lobby.setNumberOfRounds(EnvisageConstants.DEFAULT_NO_OF_ROUNDS);
+       lobby.setPlayers(playerList);
+
+        entityManager.persist(lobby);
+        entityManager.flush();
+
+        // when
+        Lobby found = lobbyRepository.findByPin(lobby.getPin());
+
+        // then
+        assertNotNull(found.getPin());
+        assertEquals(found.getPin(), lobby.getPin());
+        assertEquals(found.getPlayers(), lobby.getPlayers());
+        assertEquals(found.getRoundDuration(), lobby.getRoundDuration());
+        assertEquals(found.getNumberOfRounds(), lobby.getNumberOfRounds());
+        assertEquals(found.getGame(), lobby.getGame());
+    }
+
+
+    @Test
+    public void findByPin_failure() {
+
+        Lobby lobby = new Lobby();
+        List<Player> playerList = new ArrayList<>();
+        lobby.setPin(12345678L);
+        lobby.setRoundDuration(EnvisageConstants.DEFAULT_ROUND_DURATION_IN_SECONDS);
+        lobby.setNumberOfRounds(EnvisageConstants.DEFAULT_NO_OF_ROUNDS);
+        lobby.setPlayers(playerList);
+
+        entityManager.persist(lobby);
+        entityManager.flush();
+
+        // when
+        Lobby found = lobbyRepository.findByPin(123L);
+
+        // then
+        assertNull(found);
+    }
+}
