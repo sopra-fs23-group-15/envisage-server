16a10c86bfccbefbc44b6f52b866fd050f85ed86
nikita-uzh <nikitaammini.amitabh@uzh.ch>
2023-04-27 21:13:08 +0200

    refactored PLayerImageDoesNotExist to PlayerImageDoesNotExistException.java

--- src/main/java/ch/uzh/ifi/hase/soprafs23/exceptions/PlayerImageDoesNotExist.java
+++
@@ -1,7 +0,0 @@
-package ch.uzh.ifi.hase.soprafs23.exceptions;
-
-public class PlayerImageDoesNotExist extends RuntimeException{
-    public PlayerImageDoesNotExist(long imageId){
-        super(String.format("No image with imageId %s  exists", imageId));
-    }
-}
